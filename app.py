803aec2861f62acf3de57505297d78777b6d9254
Bryan <bryan54045@gmail.com>
2024-06-29 12:45:06 +0800

    Change the db to firebase

--- app.py
+++ app.py
@@ -1,85 +1,87 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, abort
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, init_db, User, Message
-from forms import LoginForm, RegisterForm
+import pyrebase
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-init_db(app)
+# Firebase configuration
+firebase_config = {
+    "apiKey": "AIzaSyBRw-lOvRw0R7c6GgyWWFRACdzgYKYn7qI",
+    "authDomain": "flask-chat-room.firebaseapp.com",
+    "databaseURL": "https://flask-chat-room-default-rtdb.firebaseio.com",
+    "projectId": "flask-chat-room",
+    "storageBucket": "flask-chat-room.appspot.com",
+    "messagingSenderId": "60784813085",
+    "appId": "1:60784813085:web:54594626a95baa37a992f4"
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+db = firebase.database()
+auth = firebase.auth()
+
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 
+class User(UserMixin):
+    def __init__(self, uid, username, is_admin=False):
+        self.id = uid
+        self.username = username
+        self.is_admin = is_admin
+
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    user_data = db.child("users").child(user_id).get().val()
+    if user_data:
+        return User(user_id, user_data['username'], user_data.get('is_admin', False))
+    return None
 
 @app.route('/')
 @login_required
 def chat():
-    messages = Message.query.order_by(Message.timestamp.asc()).all()
+    messages = db.child("messages").get().val()
+    if messages:
+        messages = [{'id': k, **v} for k, v in messages.items()]
+        messages.sort(key=lambda x: x['timestamp'])
+    else:
+        messages = []
     return render_template('chat.html', messages=messages)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and check_password_hash(user.password, form.password.data):
-            login_user(user)
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            user_data = db.child("users").child(user['localId']).get().val()
+            login_user(User(user['localId'], user_data['username'], user_data.get('is_admin', False)))
             return redirect(url_for('chat'))
-        flash('Invalid username or password')
-    return render_template('login.html', form=form)
+        except:
+            flash('Invalid email or password')
+    return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data)
-        new_user = User(username=form.username.data, password=hashed_password)
-        if new_user.username == 'Teacher':
-            new_user.is_admin = True
-        db.session.add(new_user)
-        db.session.commit()
-        flash('Account created successfully')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-
-@app.route('/delete_message/<int:message_id>', methods=['POST'])
-@login_required
-def delete_message(message_id):
-    if not current_user.is_admin:
-        abort(403)
-    message = Message.query.get_or_404(message_id)
-    db.session.delete(message)
-    db.session.commit()
-    flash('Message deleted successfully')
-    return redirect(url_for('chat'))
-
-@app.route('/delete_user/<int:user_id>', methods=['POST'])
-@login_required
-def delete_user(user_id):
-    if not current_user.is_admin:
-        abort(403)
-    user = User.query.get_or_404(user_id)
-    if user.username == 'Teacher':
-        flash('Cannot delete the admin account')
-        return redirect(url_for('chat'))
-    db.session.delete(user)
-    db.session.commit()
-    flash('User deleted successfully')
-    return redirect(url_for('chat'))
-
-@app.route('/users')
-@login_required
-def users():
-    if not current_user.is_admin:
-        abort(403)
-    users = User.query.all()
-    return render_template('users.html', users=users)
+    if request.method == 'POST':
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        try:
+            user = auth.create_user_with_email_and_password(email, password)
+            is_admin = (username == 'Teacher')
+            db.child("users").child(user['localId']).set({
+                "username": username,
+                "email": email,
+                "is_admin": is_admin
+            })
+            flash('Account created successfully')
+            return redirect(url_for('login'))
+        except:
+            flash('Registration failed')
+    return render_template('register.html')
 
 @app.route('/logout')
 @login_required
@@ -92,12 +94,45 @@ def logout():
 def send_message():
     content = request.form.get('content')
     if content:
-        new_message = Message(content=content, user_id=current_user.id)
-        db.session.add(new_message)
-        db.session.commit()
+        db.child("messages").push({
+            "content": content,
+            "user_id": current_user.id,
+            "username": current_user.username,
+            "timestamp": datetime.utcnow().isoformat()
+        })
     return redirect(url_for('chat'))
 
+@app.route('/delete_message/<message_id>', methods=['POST'])
+@login_required
+def delete_message(message_id):
+    if current_user.is_admin:
+        db.child("messages").child(message_id).remove()
+        flash('Message deleted successfully')
+    return redirect(url_for('chat'))
+
+@app.route('/users')
+@login_required
+def users():
+    if current_user.is_admin:
+        users = db.child("users").get().val()
+        if users:
+            users = [{'id': k, **v} for k, v in users.items()]
+        else:
+            users = []
+        return render_template('users.html', users=users)
+    return redirect(url_for('chat'))
+
+@app.route('/delete_user/<user_id>', methods=['POST'])
+@login_required
+def delete_user(user_id):
+    if current_user.is_admin:
+        user_data = db.child("users").child(user_id).get().val()
+        if user_data and not user_data.get('is_admin', False):
+            db.child("users").child(user_id).remove()
+            flash('User deleted successfully')
+        else:
+            flash('Cannot delete admin user')
+    return redirect(url_for('users'))
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
